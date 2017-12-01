4d22380760ed2566e05fedfd508fa2ba376a4c4e
jon <jrinciari@or-specialties.com>
2017-12-01 15:02:50 +0000

    set up default routes

--- routes/api.js
+++ routes/api.js
@@ -5,4 +5,11 @@ router.get('/', function(req, res){
     res.send('Instructions for API Usage')
 })
 
+router.get('/imagesearch/:query', function(req, res){
+    res.send(`Searching for ${req.params.query}`)
+})
+
+router.get('/latest', function(req, res){
+    res.send('returning latest searches')
+})
 module.exports = router;
