35ed248658300ccc476e46f8b7dbc636f193f446
jon <jrinciari@or-specialties.com>
2017-12-01 18:13:15 +0000

    updated api controller to remove dependency on google-image package

--- controllers/api_controllers.js
+++ controllers/api_controllers.js
@@ -1,18 +1,10 @@
-var googleImages = require('google-images')
-const client = new googleImages('process.env.GS_CX', 'process.env.GS_KEY')
+
 
 exports.api_index = function(req, res){
     res.send('testing')
 };
 
 exports.image_search = function(req, res){
-    client.search(req.params.query)
-        .then(images => {
-            console.log(images[0])
-        })
-        .catch(err){
-            console.log(err)
-        }
 }
 
 exports.search_history = function(req, res){
