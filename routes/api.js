c9e9f82047a08bd558711eba503330749cef3488
jon <jrinciari@or-specialties.com>
2017-12-01 15:16:11 +0000

    updated routes and controllers for testing

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ var api_controller = require('../controllers/api_controllers')
 router.get('/', api_controller.api_index)
 
 router.get('/imagesearch', api_controller.search_redirect)
+
 router.get('/imagesearch/:query', api_controller.image_search)
 
 router.get('/latest', api_controller.search_history)
