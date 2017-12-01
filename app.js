414f8b992b56f07bf48637e54c5cbd53938ef585
jon <jrinciari@or-specialties.com>
2017-12-01 17:18:25 +0000

    connected to database

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-require('dotenv').config()
+var mongoose = require('mongoose');
 var index = require('./routes/index');
 var api = require('./routes/api')
-
 var app = express();
+require('dotenv').config()
+
+var mongoDB = process.env.DB_URL;
+mongoose.connect(mongoDB, {
+  useMongoClient: true
+})
+
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB Connection Error'))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
